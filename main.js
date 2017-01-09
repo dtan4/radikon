9122a08ce8568b01698e616e0e53670a982873fa
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-10 02:45:01 +0900

    Launch as menubar desktop application

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const flashLoader = require('flash-player-loader');
+const menubar = require('menubar');
 
 const path = require('path');
 const url = require('url');
@@ -13,36 +14,15 @@ let chromeFlashes = flashLoader.getAllChromeFlashVersions();
 chromeFlashes.forEach((cf) => flashLoader.addSource(cf[1]));
 flashLoader.load();
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      plugins: true,
-    },
-  });
-
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+let mb = menubar({
+  preloadWindow: true,
+  width: 800,
+  height: 600,
+  webPreferences: {
+    plugins: true,
+  },
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+mb.on('ready', () => {
+  console.log('ready!');
 });
